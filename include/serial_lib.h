612c369a714264f867c0fa384a666a1f8b95cf92
Shubhojyoti <shubho.important@gmail.com>
2015-01-14 12:42:52 +0530

    More modular structure

--- include/serial_lib.h
+++ include/serial_lib.h
@@ -5,37 +5,25 @@
 //
 
 
-
 #ifndef __ARDUINO_SERIAL_LIB_H__
 #define __ARDUINO_SERIAL_LIB_H__
 
-// uncomment this to debug reads
-
 #include <stdint.h>   // Standard types 
-#include <stdio.h>   // Standard input/output definitions 
-#include <unistd.h>   // UNIX standard function definitions 
-#include <fcntl.h>    // File control definitions 
-#include <errno.h>    // Error number definitions 
-#include <termios.h>  // POSIX terminal control definitions 
-#include <string.h>   // String function definitions 
-#include <sys/ioctl.h>
 
-class serial_lib
-{
-private:
-	int fd;
-public:
-	serial_lib(const char* serialport);
-	bool serialport_init(int baud);
-	int serialport_close();
-	int serialport_writebyte(uint8_t b);
-	int serialport_write(const char* str);
-	int serialport_read_until(char* buf, char until, int buf_max,int timeout);
-	int serialport_flush();
-	//~serial_lib();
-	
+class serial_lib{
+	private:
+		int fd;
+	public:
+		serial_lib();
+		bool isInitialized() ;
+		int serialport_init(const char* serialport, int baud);
+		int serialport_close();
+		int serialport_writebyte( uint8_t b);
+		int serialport_write( const char* str);
+		int serialport_read_until( char* buf, char until, int buf_max,int timeout);
+		int serialport_flush();
+
+
 };
-
-
 #endif
 
--- include/steer_pub.h
+++ include/steer_pub.h
@@ -1,3 +1,8 @@
+#ifndef __STEER_PUB_H__
+#define __STEER_PUB_H__
+
+#define STEERING_DEBUG
+
 #include "serial_lib.h"
 #include "crc16.h"
 #include <iostream>
@@ -7,19 +12,21 @@
 #include <math.h>
 #include <cctype>
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 //generate steering angle and steering angle velocity
 class steering_encoder
 {
 	private:
-		float angle;//handwheel angles
-		float angle_change; //handwheel angle change
-		float time_step;
+		char* packet;
+		serial_lib *serialConnection;
 	public:
 		steering_encoder();
-		void get_steer_values(char *packet); //calculate steering angles and velocities by combining hb and lb
+		void get_steer_values(float &ang, float &ang_change); //calculate steering angles and velocities by combining hb and lb
 		float handwheel_to_steering(bool radians=false); //convert angle to actual_steering_angle
-		bool crc_valid(char *packet); //check crc validity for packet
-		bool read_packet();	//read data from serial port
-		void* generate_message(); //generate msg object of std_msgs/Float type
+		bool crc_check(); //check crc validity for packet
+		void read_packet();	//read data from serial port
+		
 };
+#endif
